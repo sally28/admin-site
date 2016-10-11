40d157d2cfd56fa9030f95d2d95b1da094b24db4
Sally Chen <sallychen426@gmail.com>
2016-10-11 19:19:35 -0400

    the completed sidebar

---
+++ dist/js/script.js
@@ -0,0 +1,36 @@
+var app = angular.module("admin", ["ngRoute"])
+.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+	  .when('/main',{
+		templateUrl:"main.html",
+		controller:"MainCtrl"
+	}).
+	  when('/about',{
+		templateUrl:"about.html",
+		controller:"MainCtrl"
+	}).
+	when('/services',{
+		templateUrl:"services.html",
+		controller:"ServicesCtrl"
+	}).
+	when('/contact',{
+		templateUrl:"contact.html",
+		controller:"ContactCtrl"
+	}).
+
+	otherwise({redirectTo:'/main'});
+}])
+
+.controller('MainCtrl',['$scope',function($scope){
+    
+}])
+
+.controller('ServicesCtrl',['$scope','$http',function($scope,$http){
+    $http.get('services.json').then(function(response){
+    	$scope.services =response.data
+    });
+}]) 
+
+.controller('ContactCtrl',['$scope',function($scope){
+    
+}]); 
