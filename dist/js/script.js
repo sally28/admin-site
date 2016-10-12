a149cdc687eae8b73c1b25e1cb2d952614219443
Sally Chen <sallychen426@gmail.com>
2016-10-11 22:01:22 -0400

    added angular-route, but it still has double header.

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,20 +5,52 @@ var app = angular.module("admin", ["ngRoute"])
 		templateUrl:"main.html",
 		controller:"MainCtrl"
 	}).
-	  when('/about',{
-		templateUrl:"about.html",
+	  when('/flot',{
+		templateUrl:"flot.html",
 		controller:"MainCtrl"
 	}).
-	when('/services',{
-		templateUrl:"services.html",
+	when('/morris',{
+		templateUrl:"morris.html",
 		controller:"ServicesCtrl"
 	}).
-	when('/contact',{
-		templateUrl:"contact.html",
+	when('/tables',{
+		templateUrl:"tables.html",
 		controller:"ContactCtrl"
 	}).
-
-	otherwise({redirectTo:'/main'});
+	when('/forms',{
+		templateUrl:"forms.html",
+		controller:"ContactCtrl"
+	}).
+	when('/panels-wells',{
+		templateUrl:"panels-wells.html",
+		controller:"ContactCtrl"
+	}).
+	when('/buttons',{
+		templateUrl:"buttons.html",
+		controller:"ContactCtrl"
+	}).
+	when('/notifications',{
+		templateUrl:"notifications.html",
+		controller:"ContactCtrl"
+	}).
+	when('/typography',{
+		templateUrl:"typography.html",
+		controller:"ContactCtrl"
+	}).
+	when('/icons',{
+		templateUrl:"icons.html",
+		controller:"ContactCtrl"
+	}).
+	when('/grid',{
+		templateUrl:"grid.html",
+		controller:"ContactCtrl"
+	}).
+	when('/blank',{
+		templateUrl:"blank.html",
+		controller:"ContactCtrl"
+	})
+	
+	
 }])
 
 .controller('MainCtrl',['$scope',function($scope){
